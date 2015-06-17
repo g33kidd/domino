d9af439ae2b5f793e471dcb17e05f37701de422a
Joshua Kidd <kidd.josh.343@gmail.com>
2015-06-17 10:17:02 -0600

    Updated templates.php functions file

--- functions/templates.php
+++ functions/templates.php
@@ -6,63 +6,6 @@
  *
  * @package Domino
  */
-
-if ( ! function_exists( 'the_posts_navigation' ) ) :
-/**
- * Display navigation to next/previous set of posts when applicable.
- *
- * @todo Remove this function when WordPress 4.3 is released.
- */
-function the_posts_navigation() {
-	// Don't print empty markup if there's only one page.
-	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
-		return;
-	}
-	?>
-	<nav class="navigation posts-navigation" role="navigation">
-		<div class="nav-links">
-
-			<?php if ( get_next_posts_link() ) : ?>
-			<div class="nav-previous"><?php next_posts_link( __( 'Older posts', 'domino' ) ); ?></div>
-			<?php endif; ?>
-
-			<?php if ( get_previous_posts_link() ) : ?>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts', 'domino' ) ); ?></div>
-			<?php endif; ?>
-
-		</div><!-- .nav-links -->
-	</nav><!-- .navigation -->
-	<?php
-}
-endif;
-
-if ( ! function_exists( 'the_post_navigation' ) ) :
-/**
- * Display navigation to next/previous post when applicable.
- *
- * @todo Remove this function when WordPress 4.3 is released.
- */
-function the_post_navigation() {
-	// Don't print empty markup if there's nowhere to navigate.
-	$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
-	$next     = get_adjacent_post( false, '', false );
-
-	if ( ! $next && ! $previous ) {
-		return;
-	}
-	?>
-	<nav class="navigation post-navigation" role="navigation">
-		<div class="nav-links">
-			<?php
-				previous_post_link( '<div class="nav-previous">%link</div>', '%title' );
-				next_post_link( '<div class="nav-next">%link</div>', '%title' );
-			?>
-		</div><!-- .nav-links -->
-	</nav><!-- .navigation -->
-	<?php
-}
-endif;
-
 if ( ! function_exists( 'domino_posted_on' ) ) :
 /**
  * Prints HTML with meta information for the current post-date/time and author.
@@ -72,229 +15,33 @@ function domino_posted_on() {
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 	}
-
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
 		esc_html( get_the_date() ),
 		esc_attr( get_the_modified_date( 'c' ) ),
 		esc_html( get_the_modified_date() )
 	);
-
 	$posted_on = sprintf(
-		_x( 'Posted on %s', 'post date', 'domino' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		_x( '%s', 'post date', 'domino' ),
+		'<i class="fa fa-calendar"></i> <a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
-
 	$byline = sprintf(
-		_x( 'by %s', 'post author', 'domino' ),
+		_x( '<i class="fa fa-user"></i> %s', 'post author', 'domino' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
-
 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
-
 }
 endif;
-
-if ( ! function_exists( 'domino_entry_footer' ) ) :
-/**
- * Prints HTML with meta information for the categories, tags and comments.
- */
-function domino_entry_footer() {
-	// Hide category and tag text for pages.
-	if ( 'post' == get_post_type() ) {
-		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( __( ', ', 'domino' ) );
-		if ( $categories_list && domino_categorized_blog() ) {
-			printf( '<span class="cat-links">' . __( 'Posted in %1$s', 'domino' ) . '</span>', $categories_list );
-		}
-
-		/* translators: used between list items, there is a space after the comma */
-		$tags_list = get_the_tag_list( '', __( ', ', 'domino' ) );
-		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . __( 'Tagged %1$s', 'domino' ) . '</span>', $tags_list );
-		}
-	}
-
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		comments_popup_link( __( 'Leave a comment', 'domino' ), __( '1 Comment', 'domino' ), __( '% Comments', 'domino' ) );
-		echo '</span>';
-	}
-
-	edit_post_link( __( 'Edit', 'domino' ), '<span class="edit-link">', '</span>' );
-}
-endif;
-
-if ( ! function_exists( 'the_archive_title' ) ) :
-/**
- * Shim for `the_archive_title()`.
- *
- * Display the archive title based on the queried object.
- *
- * @todo Remove this function when WordPress 4.3 is released.
- *
- * @param string $before Optional. Content to prepend to the title. Default empty.
- * @param string $after  Optional. Content to append to the title. Default empty.
- */
-function the_archive_title( $before = '', $after = '' ) {
-	if ( is_category() ) {
-		$title = sprintf( __( 'Category: %s', 'domino' ), single_cat_title( '', false ) );
-	} elseif ( is_tag() ) {
-		$title = sprintf( __( 'Tag: %s', 'domino' ), single_tag_title( '', false ) );
-	} elseif ( is_author() ) {
-		$title = sprintf( __( 'Author: %s', 'domino' ), '<span class="vcard">' . get_the_author() . '</span>' );
-	} elseif ( is_year() ) {
-		$title = sprintf( __( 'Year: %s', 'domino' ), get_the_date( _x( 'Y', 'yearly archives date format', 'domino' ) ) );
-	} elseif ( is_month() ) {
-		$title = sprintf( __( 'Month: %s', 'domino' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'domino' ) ) );
-	} elseif ( is_day() ) {
-		$title = sprintf( __( 'Day: %s', 'domino' ), get_the_date( _x( 'F j, Y', 'daily archives date format', 'domino' ) ) );
-	} elseif ( is_tax( 'post_format' ) ) {
-		if ( is_tax( 'post_format', 'post-format-aside' ) ) {
-			$title = _x( 'Asides', 'post format archive title', 'domino' );
-		} elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) {
-			$title = _x( 'Galleries', 'post format archive title', 'domino' );
-		} elseif ( is_tax( 'post_format', 'post-format-image' ) ) {
-			$title = _x( 'Images', 'post format archive title', 'domino' );
-		} elseif ( is_tax( 'post_format', 'post-format-video' ) ) {
-			$title = _x( 'Videos', 'post format archive title', 'domino' );
-		} elseif ( is_tax( 'post_format', 'post-format-quote' ) ) {
-			$title = _x( 'Quotes', 'post format archive title', 'domino' );
-		} elseif ( is_tax( 'post_format', 'post-format-link' ) ) {
-			$title = _x( 'Links', 'post format archive title', 'domino' );
-		} elseif ( is_tax( 'post_format', 'post-format-status' ) ) {
-			$title = _x( 'Statuses', 'post format archive title', 'domino' );
-		} elseif ( is_tax( 'post_format', 'post-format-audio' ) ) {
-			$title = _x( 'Audio', 'post format archive title', 'domino' );
-		} elseif ( is_tax( 'post_format', 'post-format-chat' ) ) {
-			$title = _x( 'Chats', 'post format archive title', 'domino' );
-		}
-	} elseif ( is_post_type_archive() ) {
-		$title = sprintf( __( 'Archives: %s', 'domino' ), post_type_archive_title( '', false ) );
-	} elseif ( is_tax() ) {
-		$tax = get_taxonomy( get_queried_object()->taxonomy );
-		/* translators: 1: Taxonomy singular name, 2: Current taxonomy term */
-		$title = sprintf( __( '%1$s: %2$s', 'domino' ), $tax->labels->singular_name, single_term_title( '', false ) );
-	} else {
-		$title = __( 'Archives', 'domino' );
-	}
-
-	/**
-	 * Filter the archive title.
-	 *
-	 * @param string $title Archive title to be displayed.
-	 */
-	$title = apply_filters( 'get_the_archive_title', $title );
-
-	if ( ! empty( $title ) ) {
-		echo $before . $title . $after;
-	}
-}
-endif;
-
-if ( ! function_exists( 'the_archive_description' ) ) :
-/**
- * Shim for `the_archive_description()`.
- *
- * Display category, tag, or term description.
- *
- * @todo Remove this function when WordPress 4.3 is released.
- *
- * @param string $before Optional. Content to prepend to the description. Default empty.
- * @param string $after  Optional. Content to append to the description. Default empty.
- */
-function the_archive_description( $before = '', $after = '' ) {
-	$description = apply_filters( 'get_the_archive_description', term_description() );
-
-	if ( ! empty( $description ) ) {
-		/**
-		 * Filter the archive description.
-		 *
-		 * @see term_description()
-		 *
-		 * @param string $description Archive description to be displayed.
-		 */
-		echo $before . $description . $after;
-	}
-}
-endif;
-
-/**
- * Echoes all the social links with icons and classes
- */
 if(!function_exists('the_social_links')):
 function the_social_links() {
 	$result = "";
-	$twitter = tmod('general', 'social', 'twitter');
-	$facebook = tmod('general', 'social', 'facebook');
-	$google = tmod('general', 'social', 'google-plus');
-	$youtube = tmod('general', 'social', 'youtube');
-	$linkedin = tmod('general', 'social', 'linkedin');
-	
-	if($twitter) {
-		$result .= '<a href="'.$twitter.'" class="social-icon twitter"><i class="fa fa-twitter"></i></a>';
+	$services = ['twitter', 'facebook', 'linkedin', 'google-plus', 'youtube'];
+	foreach ($services as $service) {
+		$link = tmod('general', 'social', $service);
+		if(!empty($link)) {
+			$result .= '<a href="'. $link .'" class="social-icon '. $service .'"><i class="fa fa-'. $service .'"></i></a>';
+		}
 	}
-
-	if($facebook) {
-		$result .= '<a href="'.$facebook.'" class="social-icon facebook"><i class="fa fa-facebook"></i></a>';
-	}
-
-	if($youtube) {
-		$result .= '<a href="'.$youtube.'" class="social-icon youtube"><i class="fa fa-youtube"></i></a>';
-	}
-
-	if($google) {
-		$result .= '<a href="'.$google.'" class="social-icon google-plus"><i class="fa fa-google-plus"></i></a>';
-	}
-
-	if($linkedin) {
-		$result .= '<a href="'.$linkedin.'" class="social-icon linkedin"><i class="fa fa-linkedin"></i></a>';
-	}
-
 	echo $result;
 }
 endif;
-
-/**
- * Returns true if a blog has more than 1 category.
- *
- * @return bool
- */
-function domino_categorized_blog() {
-	if ( false === ( $all_the_cool_cats = get_transient( 'domino_categories' ) ) ) {
-		// Create an array of all the categories that are attached to posts.
-		$all_the_cool_cats = get_categories( array(
-			'fields'     => 'ids',
-			'hide_empty' => 1,
-
-			// We only need to know if there is more than one category.
-			'number'     => 2,
-		) );
-
-		// Count the number of categories that are attached to the posts.
-		$all_the_cool_cats = count( $all_the_cool_cats );
-
-		set_transient( 'domino_categories', $all_the_cool_cats );
-	}
-
-	if ( $all_the_cool_cats > 1 ) {
-		// This blog has more than 1 category so domino_categorized_blog should return true.
-		return true;
-	} else {
-		// This blog has only 1 category so domino_categorized_blog should return false.
-		return false;
-	}
-}
-
-/**
- * Flush out the transients used in domino_categorized_blog.
- */
-function domino_category_transient_flusher() {
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-		return;
-	}
-	// Like, beat it. Dig?
-	delete_transient( 'domino_categories' );
-}
-add_action( 'edit_category', 'domino_category_transient_flusher' );
-add_action( 'save_post',     'domino_category_transient_flusher' );
